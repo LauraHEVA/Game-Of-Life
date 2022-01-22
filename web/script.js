27efdbeae1d8c66ed19c4f4ff6957f74847fcd29
Laura Hernández <lhvarela@gmail.com>
2022-01-22 12:41:56 +0100

    Added function to check neighbours vertically

--- web/script.js
+++ web/script.js
@@ -2,6 +2,7 @@ const arrayAllCells = document.querySelectorAll(".board__column__cell");
 const arrayCellAlives = [];
 const startButton = document.getElementById("start");
 let isGameRunning = false;
+let numberOfNeighbours;
 
 for (const cell of arrayAllCells) {
   cell.addEventListener("click", () => {
@@ -17,4 +18,18 @@ startButton.addEventListener("click", () => {
   return isGameRunning;
 });
 
-function checkNeighbours(idCell) {}
+function checkNeighboursA(idCell) {
+  const numberToCheckA = idCell + 1;
+  if (arrayCellAlives.includes(numberToCheckA)) {
+    numberOfNeighbours++;
+  }
+  return numberOfNeighbours;
+}
+
+function checkNeighboursB(idCell) {
+  const numberToCheckB = idCell + 10;
+  if (arrayCellAlives.includes(numberToCheckB)) {
+    numberOfNeighbours++;
+  }
+  return numberOfNeighbours;
+}
--- web/script.test.js
+++ web/script.test.js
@@ -1,7 +1,24 @@
-const arrayAllCells = document.querySelectorAll(".board__column__cell");
+const arrayCellAlives = [1823, 1824];
+const isGameRunning = false;
+let numberOfNeighbours = 0;
 
-for (const celda of arrayAllCells) {
-  celda.addEventListener("click", () => {
-    document.getElementById(celda.id).style.background = "#ff006e";
-  });
+function checkNeighbours(idCell) {
+  const numberToCheckVertical = idCell + 1;
+  if (arrayCellAlives.includes(numberToCheckVertical)) {
+    numberOfNeighbours++;
+  }
+  return numberOfNeighbours;
 }
+
+describe("Given a checkNeighbours function", () => {
+  describe("When it receives a 1823", () => {
+    test("Then it should return numberOfNeighbours 1", () => {
+      const id = 1823;
+      const expectedNeighbour = 1;
+
+      const isNeighbour = checkNeighbours(id);
+
+      expect(isNeighbour).toBe(expectedNeighbour);
+    });
+  });
+});
