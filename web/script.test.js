87a0415d02d16632e40e95814d7c404c9a496926
Laura Hernández <lhvarela@gmail.com>
2022-01-23 10:19:03 +0100

    Tests for checkOneCell function run ok

--- web/script.test.js
+++ web/script.test.js
@@ -1,42 +1,7 @@
 const arrayCellAlives = [1823, 1824, 1825, 2209, 2210, 2310];
-let numberOfNeighbours = 0;
-
-function resetNumberOfNeighbours() {
-  numberOfNeighbours = 0;
-}
-
-function checkNeighbours(idCell) {
-  if (arrayCellAlives.includes(idCell + 1)) {
-    numberOfNeighbours++;
-  }
-  return numberOfNeighbours;
-}
-
-describe("Given a checkNeighbours function", () => {
-  describe("When it receives a 1823", () => {
-    test("Then it should return numberOfNeighbours 1", () => {
-      const id = 1823;
-      const expectedNeighbour = 1;
-
-      const isNeighbour = checkNeighbours(id);
-
-      expect(isNeighbour).toBe(expectedNeighbour);
-    });
-  });
-  describe("When it receives a 1824", () => {
-    test("Then it should return numberOfNeighbours 2", () => {
-      const id = 1823;
-      const expectedNeighbour = 2;
-
-      const isNeighbour = checkNeighbours(id);
-
-      expect(isNeighbour).toBe(expectedNeighbour);
-    });
-  });
-});
 
 function checkOneCell(idCell) {
-  resetNumberOfNeighbours();
+  let numberOfNeighbours = 0;
   if (arrayCellAlives.includes(idCell - 1)) {
     numberOfNeighbours++;
   }
@@ -63,6 +28,29 @@ function checkOneCell(idCell) {
   }
   return numberOfNeighbours;
 }
+
+describe("Given a checkOneCell function", () => {
+  describe("When it receives a 1823", () => {
+    test("Then it should return numberOfNeighbours 1", () => {
+      const id = 1823;
+      const expectedNeighbour = 1;
+
+      const isNeighbour = checkOneCell(id);
+
+      expect(isNeighbour).toBe(expectedNeighbour);
+    });
+  });
+  describe("When it receives a 1824", () => {
+    test.only("Then it should return numberOfNeighbours 2", () => {
+      const id = 1824;
+      const expectedNeighbour = 2;
+
+      const isNeighbour = checkOneCell(id);
+
+      expect(isNeighbour).toBe(expectedNeighbour);
+    });
+  });
+});
 
 describe("Given a checkOneCell function", () => {
   describe("When it receives a 1923", () => {
